7ff75931d9768acb559eb8c107711c23332f4025
Anirudh r k <menonani7@gmail.com>
2023-12-13 15:45:09 +0530

    using childern in prop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups"; // Assuming AllMeetups.js contains the AllMeetups component
 import Favourites from "./pages/Favourites"; // Assuming Favourites.js contains the Favourites component
-import MainNavigation from "./components/MainNavigation";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
