26bad4dacc9898854a9bdef8d9f9193682af95c0
Anirudh r k <menonani7@gmail.com>
2023-12-14 14:26:29 +0530

    complete form handling and data retrieval

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,27 +1,50 @@
-import Card from '../ui/Card';
-import classes from './NewMeetup.module.css';
+import Card from "../ui/Card";
+import classes from "./NewMeetup.module.css";
+import { useRef } from "react";
 
 function NewMeetupForm() {
+  //listened even which is triggered on button click
 
+  //user ref is used to ger the value of the referenced object
+  //best practice is to use the ref for reading input
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descInputRef = useRef();
+  function submitHandler(event) {
+    event.preventDefault();
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDesc = descInputRef.current.value;
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      desc: enteredDesc,
+    };
+    //send it to server to store in db
+    console.log(meetupData);
+  }
 
   return (
     <Card>
-      <form className={classes.form} >
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='title'>Meetup Title</label>
-          <input type='text' required id='title' />
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='image'>Meetup Image</label>
-          <input type='url' required id='image' />
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='address'>Address</label>
-          <input type='text' required id='address' />
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='desc'>Description</label>
-          <textarea id='desc' required rows='5'></textarea>
+          <label htmlFor="desc">Description</label>
+          <textarea id="desc" required rows="5" ref={descInputRef}></textarea>
         </div>
         <div className={classes.actions}>
           <button>Add Meetup</button>
